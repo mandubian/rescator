cd940824e1d75bc2fe38e2dcb7db2b2b12a0c2f8
Mandubian <pascal.voitot@mandubian.org>
2011-12-01 22:28:38 +0100

    added operator /

--- src/main/scala/rescator.scala
+++ src/main/scala/rescator.scala
@@ -21,14 +21,32 @@ package rescator {
 		implicit def handlertoRescatorHandler(subject: HandlerVerbs) = new RescatorHandler(subject)
 		implicit def requesttoRescatorHandler(request:Request) = new RescatorHandler(request)
 		implicit def stringToRescatorHandler(str: String) = new RescatorHandler(new Request(str))
+		
+		implicit def symToRescatorJsonSymOp(sym:Symbol) = RescatorChild(sym, None)
 	}
 
+	case class JsonXPath(sym:Symbol, parent:Option[Obj] = None) {
+	
+	}
+	
+	trait RestHandler {
+		def >>>[T](paths:JsonXPath*)
+	}
 	
 	case class RescatorHandler(subject:HandlerVerbs) {
 		import dispatch.json.JsHttp._
 		import dispatch.Http
-		def >>> [T](block: json.Js.JsF[T]) = Http(subject ># block)		
+		def >>> [T](block: json.Js.JsF[T]) = Http(subject ># block)
 	}
 	
-	
+	case class RescatorChild(sym:Symbol, parent:Option[Obj]) {
+	  import dispatch.json.Property
+	  
+	  def as[T](ext:Extract[T]) = {
+	    new Child[T, Property[T]](parent, Property(sym, ext))
+	  }
+	  
+	  def / [T](childsym: Symbol) = RescatorChild(childsym, Some(new Obj(sym)))
+	}
+	// (alpha, delta) = GET("http://...") >>> ( 'alpha, 'beta/'delta )
 }
