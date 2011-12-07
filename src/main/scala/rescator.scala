2928131f86c55f2774987be7e96d003740e910c8
Mandubian <pascal.voitot@mandubian.org>
2011-12-07 07:25:41 +0100

    added options to JsonXPath and operator \\ seems to work now and added jsValue direct mapper but doesn't work yet

--- src/main/scala/rescator.scala
+++ src/main/scala/rescator.scala
@@ -12,6 +12,15 @@ package object rescator extends ImplicitHandlerVerbs with Js{
 	def POST(req:Request):Request = req.POST
 	
 	def JS(jsString:String) = Js(jsString)
+	
+	val R = ROOT
+	val HEAD = ROOT
+	val __ = ROOT
+	
+	// ROOT\'test'\'test2 
+	// HEAD\'test'\'test2 
+	// R\'test'\'test2
+	// __\'test'\'test2
 }
 
 package rescator {
@@ -19,9 +28,11 @@ package rescator {
 	trait ImplicitHandlerVerbs {		
 		import dispatch.json.Js._
 		
-		implicit def symToJsonXPath(sym:Symbol) = JsonXPath(None, sym)
+		implicit def symToJsonXPath(sym:Symbol) = \(ROOT, sym)
 		implicit def stringToJsValue(str:String) = Js(str)
-
+		
+		implicit def jsValueToRescatorJsonHandler(jsValue: JsValue) = new RescatorJsonHandler(jsValue)			
+		
 		implicit def handlertoRescatorHandler(subject: HandlerVerbs) = new RescatorHttpHandler(subject)
 		implicit def requesttoRescatorHandler(request:Request) = new RescatorHttpHandler(request)
 		implicit def stringToRescatorHandler(str: String) = new RescatorHttpHandler(new Request(str))			
@@ -31,16 +42,26 @@ package rescator {
 	// JsonXPath(JsonXPath('parent), 'child)
 	// JsonXPath(JsonXPath(JsonXPath('parent), 'child), 'child2)
 	
+	object Flag extends Enumeration {
+	  val RECURSIVE = Value
+	}
+	
 	// { "parent" : { "child1" : { "child11" : "blabla" } } } 
-	// /:parent/child1/child11
-	// //:parent/child1/child11
-	case class JsonXPath(parent:Option[JsonXPath] = None, sym:Symbol) {
+	// ROOT\parent\child1\child11
+	// ROOT\parent\child1\child11
+	case class JsonXPath(parent:Option[JsonXPath] = Some(ROOT), sym:Symbol) {
 		type JsF[T] = JsValue => T
 		
-		//def \ (childSym: Symbol)  = JsonXPath(Some(this), childSym)
+		private[rescator] var options = Set[Flag.Value]()
 		
-		def \ (childSym: Symbol)  = JsonXPath(Some(this), childSym)
+		def \ (childSym: Symbol)  = new \(this, childSym)
 				
+		def \\ (childSym: Symbol)  = {
+		  val jxp = new \(this, childSym)
+		  jxp.options += Flag.RECURSIVE
+		  jxp
+		}
+		
 		def parentOf(childSym: Symbol) = this \ childSym
 				
 		def as[T](ext:Extract[T]):JsF[T] = {		  
@@ -59,28 +80,37 @@ package rescator {
 		}
 	}
 	
-	/*final case class \(par:Option[JsonXPath], override val sym:Symbol) 
-		extends JsonXPath(par, sym) {
-	  def this(parentSym:Symbol, sym:Symbol) = this(Some(JsonXPath(None, parentSym)), sym)	   
-	}*/
+	case class \(par:JsonXPath, override val sym:Symbol) 
+		extends JsonXPath(Some(par), sym)
 	
-	object \ {
+	case object ROOT extends JsonXPath(None, 'ROOT) 
+
+	class \\(par:JsonXPath, override val sym:Symbol)
+		extends JsonXPath(Some(par), sym) {
+	  options += Flag.RECURSIVE
+	}
+	object \\ {
+	  def unapply(jxp:JsonXPath):Option[(JsonXPath, Symbol)] = {
+	    jxp match {
+	      case e @ \(par, sym) if e.options.contains(Flag.RECURSIVE) => Some((par, sym))
+	      case _ => None
+	    }
+	  }
+	}
+/*	object \ {
 	  //def apply(parentSym:Symbol, sym:Symbol) = new \(parentSym, sym)
 	  def unapply(jxp:JsonXPath):Option[(Symbol, Symbol)] = {
 	    if(jxp.parent.isDefined) Some((jxp.parent.get.sym, jxp.sym))
 	    else None
 	  }
 
-	}
+	}*/
 	
-	trait HttpHandler {
-		import dispatch.Http
-		protected def callHttp[T](subject:HandlerVerbs)(block: (java.io.InputStream, String) => T):T = Http(subject >> block)  
-
+	trait JsonHandler {
 		// a simple extractor wrapper function taking a JsValue and returning another type
 		type JsF[T] = JsValue => T
 
-		def >>>[OS <: java.io.OutputStream](out: OS)
+		//def >>>[OS <: java.io.OutputStream](out: OS)
 		def >>>[T](block: JsF[T]):T
 		def >>>[T1, T2](block1: JsF[T1], block2: JsF[T2]):(T1, T2)
 		def >>>[T1, T2, T3](block1: JsF[T1], block2: JsF[T2], block3: JsF[T3]):(T1, T2, T3)
@@ -88,7 +118,37 @@ package rescator {
 		def >>>[T1, T2, T3, T4, T5](block1: JsF[T1], block2: JsF[T2], block3: JsF[T3], block4: JsF[T4], block5: JsF[T5]): (T1, T2, T3, T4, T5)
 	}
 	
-	class RescatorHttpHandler(subject:HandlerVerbs) extends HttpHandler {
+	trait HttpHandler {
+		import dispatch.Http
+		protected def callHttp[T](subject:HandlerVerbs)(block: (java.io.InputStream, String) => T):T = Http(subject >> block)  
+
+	}
+	
+	class RescatorJsonHandler(jsValue: JsValue) extends JsonHandler {
+		def >>>[T](block: JsF[T]) = {
+			block(jsValue)
+		}
+		
+		def >>>[T1, T2](block1: JsF[T1], block2: JsF[T2]) = {
+			(block1(jsValue), block2(jsValue))
+		}
+		
+		def >>>[T1, T2, T3](block1: JsF[T1], block2: JsF[T2], block3: JsF[T3]) = {
+			(block1(jsValue), block2(jsValue), block3(jsValue))
+		}
+
+		def >>>[T1, T2, T3, T4](block1: JsF[T1], block2: JsF[T2], block3: JsF[T3], block4: JsF[T4]) = {
+			(block1(jsValue), block2(jsValue), block3(jsValue), block4(jsValue))
+		}
+
+		def >>>[T1, T2, T3, T4, T5](block1: JsF[T1], block2: JsF[T2], block3: JsF[T3], block4: JsF[T4], block5: JsF[T5]) = {
+			(block1(jsValue), block2(jsValue), block3(jsValue), block4(jsValue), block5(jsValue))
+		}
+	
+	}
+
+	
+	class RescatorHttpHandler(subject:HandlerVerbs) extends JsonHandler with HttpHandler {
 		import dispatch.json.JsHttp._
 		def >>> [OS <: java.io.OutputStream](out: OS) = subject >>> out
 		 
